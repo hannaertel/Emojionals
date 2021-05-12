fda738e55462abb1728e8cd48b4047c7b0c719b3
Hanna Ertel <hannaertel@yahoo.com>
2021-05-11 22:53:04 -0500

    Initial Commit

---
+++ Emojionals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emojionals
+//
+//  Created by Hanna Ertel on 5/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
